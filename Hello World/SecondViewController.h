02748baffcb0d443a8a6b9dcf1cf8f6637475b8b
Ruben Roques <rroques@cydrasoft.com>
2012-02-01 22:02:05 +0000

    Initial Commit

---
+++ Hello World/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  Hello World
+//
+//  Created by Z on 01/02/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
