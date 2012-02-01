48046e0a73caf9d27d5e018bc38509850d8cf598
Ruben Roques <rroques@cydrasoft.com>
2012-02-01 22:39:23 +0000

    New class, Hello Class

---
+++ Hello World/HelloClass.h
@@ -0,0 +1,15 @@
+//
+//  HelloClass.h
+//  Hello World
+//
+//  Created by Z on 01/02/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HelloClass : NSObject
+
+@property (nonatomic, strong)NSString *nome;
+
+@end
---
+++ Hello World/HelloClass.m
@@ -0,0 +1,15 @@
+//
+//  HelloClass.m
+//  Hello World
+//
+//  Created by Z on 01/02/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "HelloClass.h"
+
+@implementation HelloClass
+
+@synthesize nome=_nome;
+
+@end
